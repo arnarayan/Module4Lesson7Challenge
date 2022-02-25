1ec3727c93f33eb3e803881e265e6c401b4e5bb6
Anand.Narayan <arnarayan@gmail.com>
2022-02-24 18:52:39 -0700

    Initial Commit

---
+++ Module4Lesson7Challenge/Module4Lesson7ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module4Lesson7ChallengeApp.swift
+//  Module4Lesson7Challenge
+//
+//  Created by Anand Narayan on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct Module4Lesson7ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
